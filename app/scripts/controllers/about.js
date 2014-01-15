8ebeaf3b0e5013c770a026f2f43e0556f252e2fd
Christophe <christophe.rosset@4d.com>
2014-01-15 11:17:42 +0100

    added test calls to catalog

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -2,7 +2,10 @@
 
 angular.module('angularWakandaFrontApp')
         .controller('AboutCtrl', ['$http','$scope',function($http,$scope) {
-            $scope.xhrToWakanda = function(){
+            $scope.url1 = "http://127.0.0.1:8081/rest/$catalog/Products/";
+            $scope.url2 = "http://127.0.0.1:9000/rest/$catalog/Products/";
+            $scope.xhrToWakanda = function(url){
+                console.log(url);
 //                var xhr = new XMLHttpRequest();
 //                xhr.open("GET", "http://127.0.0.1:8081", true);
 //                xhr.onreadystatechange = function(url) {
@@ -11,11 +14,14 @@ angular.module('angularWakandaFrontApp')
 //                xhr.send();
                 $http({
                     method : 'GET',
-                    url : $scope.url
+                    url : url
                 }).success(function(data){
+                    $scope.error = false;
+                    $scope.result = JSON.stringify(data,null,"\t");
                     console.log(data);
                 }).error(function(error){
-                    console.log(error);
+                    $scope.error = true;
+                    $scope.result = "An error occured it must be a 'Access-Control-Allow-Origin' one, please watch the console.";
                 });
             };
         }]);
