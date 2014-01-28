d381944fb8401635facbb4b0f6fb290247d98795
Christophe Rosset <tophe@topheman.com>
2014-01-28 15:20:55 +0100

    $create

--- app/scripts/services/angular-wakanda-connector/angular-wakanda-connector.js
+++ app/scripts/services/angular-wakanda-connector/angular-wakanda-connector.js
@@ -1,3 +1,5 @@
+//var g;
+
 (function() {
   'use strict';
 
@@ -60,15 +62,60 @@
 
       /** private necessary objects */
 
-      var wakEntityCollection = function() {
-
-      };
+//      var WakEntityCollection = function(wakEntityCollection, entityCollectionMethods) {
+//        wakEntityCollection = (wakEntityCollection && wakEntityCollection.length > 0 ? wakEntityCollection : false) || Object.create( Array.prototype );
+//        wakEntityCollection = (Array.apply( wakEntityCollection, wakEntityCollection ) || wakEntityCollection);
+//        WakEntityCollection.injectClassMethods( wakEntityCollection, entityCollectionMethods );
+//        return wakEntityCollection;
+//      };
+//      
+//      WakEntityCollection.injectClassMethods = function( wakEntityCollection, entityCollectionMethods ){
+//        var method, originalPush;
+//        //overload push
+//        originalPush = Array.prototype.push;
+//        WakEntityCollection.prototype.push = function(){
+//          console.log('pushing',arguments);
+//          originalPush.apply(this, arguments);
+//        };
+//        // Loop over all the prototype methods and add them to the new wakEntityCollection
+//        for (method in WakEntityCollection.prototype){
+//          // Make sure this is a local method.
+//          if (WakEntityCollection.prototype.hasOwnProperty( method )){
+//            // Add the method to the collection.
+//            wakEntityCollection[ method ] = WakEntityCollection.prototype[ method ];
+//          }
+//        }
+//        //then add the EntityCollectionMethods
+//        for (method in entityCollectionMethods){
+//          // Make sure this is a local method.
+//          if (entityCollectionMethods.hasOwnProperty( method )){
+//            // Add the method to the collection.
+//            wakEntityCollection[ method ] = entityCollectionMethods[ method ];
+//          }
+//        }
+//      };
+//      
+//      WakEntityCollection.prototype.$fetch = function(){
+//        console.log('Not yet implemented $fetch method');
+//      };
+//      
+//      g = WakEntityCollection;
+//
+//      var wakEntityMethods = {
+//        $save : function(){
+//          console.log('Not yet implemented $save method');
+//        },
+//        $remove : function(){
+//          console.log('Not yet implemented $remove method');
+//        }
+//      };
 
       /** event transformation part */
 
       var wakToAngular = {
         prepareWAF: function() {
-          WAF.DataClass.prototype.$find = $$find2;
+          WAF.DataClass.prototype.$find = $$find;
+          WAF.DataClass.prototype.$create = $$create;
         },
         transformDatastore: function(dataStore) {
           var dataClass;
@@ -82,10 +129,18 @@
         },
         transformQueryEvent: function(event) {
           var result,
-                  parsedXHRResponse;
+              parsedXHRResponse;
           parsedXHRResponse = JSON.parse(event.XHR.response);
           result = parsedXHRResponse.__ENTITIES;
+          result.map(function(pojo){
+            console.log(pojo, event.result);
+          });
           result._collection = event.result;
+          
+//          result = result.map(function(pojo) {
+//             var entity = result._collection.getByKey(pojo.__ID);
+//             pojo._entity = entity;
+//          });
           //add methods on result
 
           event.result = result;
@@ -98,12 +153,43 @@
         transformEntityArray: function(entityArray) {
 
         },
-        transformWakAsyncToAngularPromise: function() {
-
+        addFrameworkMethodsToPOJO: function(pojo) {
+          pojo.$save = $WakEntityMethods.$$save;
+          pojo.$remove = $WakEntityMethods.$$remove;
         }
       };
 
       /** public methods */
+      
+      /**
+       * Applied to a WakEntity (POJO inside array result to what we add methods)
+       */
+      var $WakEntityMethods = {
+        $$save : function(){
+          console.log("$save() not yet implemented");
+        },
+        $$remove : function(){
+          console.log("$remove() not yet implemented");
+        }
+      };
+
+      /**
+       * Applied to WAF.DataClass.prototype
+       * 
+       * @argument {Object} Simple JS object matching the dataclass representation
+       * @returns {Object} POJO with $_entity pointer to WAF.Entity
+       */
+      var $$create = function(data){
+        var entity = this.newEntity();
+        for (var key in data){
+          if(data.hasOwnProperty(key)){
+            entity[key].setValue(data[key]);
+          }
+        }
+        data.$_entity = entity;
+        wakToAngular.addFrameworkMethodsToPOJO(data);
+        return data;
+      };
 
       /**
        * 
@@ -111,57 +197,6 @@
        * @returns {$q.promise}
        */
       var $$find = function(options) {
-        var deferred, wakOptions = {}, attributeList = null;
-        //input check
-        if (!options || typeof options !== "object") {
-          throw new Error("Please pass an object as options");
-        }
-        //prepare options / map to the WAF.DataStore.toArray() signature
-        if (options.select) {
-          attributeList = options.select;
-        }
-        if (options.filter) {
-          wakOptions.filterQuery = options.filter;
-        }
-        if (options.params) {
-          wakOptions.params = options.params;
-        }
-        if (options.orderBy) {
-          wakOptions.orderby = options.orderBy;// !!! watch the case
-        }
-        if (typeof options.offset !== "undefined") {
-          wakOptions.skip = options.offset;
-        }
-        if (typeof options.limit !== "undefined") {
-          wakOptions.top = options.limit;
-        }
-        if (typeof options.pageSize !== "undefined") {// !!! no pageSize on toArray
-          wakOptions.pageSize = options.pageSize;
-        }
-        //prepare the promise
-        deferred = $q.defer();
-        wakOptions.onSuccess = function(event) {
-          rootScopeSafeApply(function() {
-            deferred.resolve(event);
-          });
-        };
-        wakOptions.onError = function(event) {
-          rootScopeSafeApply(function() {
-            deferred.reject(event);
-          });
-        };
-        //make the call
-        options = null;
-        this.toArray(attributeList, wakOptions);
-        return deferred.promise;
-      };
-
-      /**
-       * 
-       * @param {Object} options
-       * @returns {$q.promise}
-       */
-      var $$find2 = function(options) {
         var deferred, wakOptions = {}, query = null;
         //input check
         if (!options || typeof options !== "object") {
