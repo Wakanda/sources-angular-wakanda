902bac055c9241641962614031c44f7a76b031cf
Christophe Rosset <tophe@topheman.com>
2014-02-06 11:12:51 +0100

    removed WAF/extra-extends.js

--- app/scripts/controllers/example1.js
+++ app/scripts/controllers/example1.js
@@ -4,35 +4,5 @@ var ds, products, queryResult;
 
 angular.module('angularWakandaFrontApp')
         .controller('Example1Ctrl', ['$scope', 'wakConnectorService', function($scope, wakConnectorService) {
-            ds = wakConnectorService.getDatastore();
-
-            $scope.query = 'name = "*"';
-
-            $scope.products = [];
-
-            $scope.$watch('query', function(query) {
-              if (!query) {
-                query = 'name = *';
-              }
-
-              ds.Product.query(query, {
-                onSuccess: function(result) {
-                  $scope.$apply(function() {
-                    queryResult = result;
-                    products = $scope.products = result.result.flatten();
-                  });
-                  console.log('$scope.products (inside onSuccess)', $scope.products);
-                }
-              });
-
-            });
-
-//            ds.Products.query('name = "po*"',{
-//                onSuccess:function(result){
-//                    products = $scope.products = result.result.flatten();
-//                    $scope.$apply();
-//                    console.log('$scope.products (inside onSuccess)',$scope.products);
-//                }
-//            });
-
+            
           }]);
