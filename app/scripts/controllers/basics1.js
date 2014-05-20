de2c88d40b46343b036288a67ae5f131a9faf3e6
Christophe Rosset <tophe@topheman.com>
2014-05-20 14:41:20 +0200

    nestedCollection : $totalCount, $more, $nextPage, $prevPage

--- app/scripts/controllers/basics1.js
+++ app/scripts/controllers/basics1.js
@@ -7,6 +7,8 @@ angular.module('angularWakandaFrontApp')
     
     ds = wakConnectorService.getDatastore();
     
+    /** comp1 */
+      
     $scope.findComp1 = function(){
       $scope.comp1 = comp1 = ds.Company.$find({});
       comp1.$promise.then(function(){
@@ -14,8 +16,8 @@ angular.module('angularWakandaFrontApp')
       });
     };
     
-    $scope.dispComp1Staff = function(){
-      console.log('comp1[2].staff',comp1[2].staff);
+    $scope.dispComp1StaffInfos = function(){
+      console.log('comp1[2].staff',comp1[2].staff,'comp1[2].staff.$query',comp1[2].staff.$query,'comp1[2].staff.$totalCount',comp1[2].staff.$totalCount,'comp1[2].staff.length',comp1[2].staff.length);
     };
     
     $scope.isLoadedComp1Staff = function(){
@@ -30,6 +32,33 @@ angular.module('angularWakandaFrontApp')
         console.groupEnd();
       },function(e){
         console.error(e);
+      });
+    };
+    
+    /** comp2 */
+    
+    $scope.dispComp2StaffInfos = function(){
+      console.log('comp1[15].staff',comp1[15].staff,'comp1[15].staff.$query',comp1[15].staff.$query,'comp1[15].staff.$totalCount',comp1[15].staff.$totalCount,'comp1[15].staff.length',comp1[15].staff.length);      
+    };
+    
+    $scope.isLoadedComp2Staff = function(){
+      console.log('comp1[15].staff.$isLoaded()',comp1[15].staff.$isLoaded());
+    };
+    
+    $scope.fetchComp2Staff = function(){
+      comp1[15].staff.$fetch().then(function(e){
+        console.group('They are the same (the first is raw, the second is passed inside the success of the promise');
+        console.log('comp1[15].staff',comp1[15].staff);
+        console.log('e',e);
+        console.groupEnd();
+      },function(e){
+        console.error(e);
+      });
+    };
+    
+    $scope.moreComp2Staff = function(){
+      comp1[15].staff.$more().then(function(e){
+        console.log('$more','e',e);
       });
     };
     
