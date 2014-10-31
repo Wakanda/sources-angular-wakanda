2cc7373c41a716346f842c93c5f5a3ed2fcba8b3
Christophe Rosset <tophe@topheman.com>
2014-10-31 17:42:32 +0100

    dev to reproduce bug #WAK0089863

--- app/scripts/controllers/bugTest3.js
+++ app/scripts/controllers/bugTest3.js
@@ -1,34 +1,40 @@
 'use strict';
 
-var ds,_;
+var ds, _;
 
 angular.module('angularWakandaFrontApp')
-        .controller('BugTest3Ctrl', ['$scope','$route','$wakanda','$q',function($scope,$route,$wakanda,$q) {
-            
-            var employeesToSave = [], companiesToSave = [], i;
-            
-            $scope.display = function(){
-              $scope.employees = $wakanda.$ds.Employee.$find({
-                filter:'firstName = _*'
+        .controller('BugTest3Ctrl', ['$scope', '$route', '$wakanda', '$q', function ($scope, $route, $wakanda, $q) {
+
+            ds = $wakanda.$ds;
+
+            var myNewEmployeeCollection = [], myNewEmployeeTmp, myNewCompany, i;
+
+            $scope.display = function () {
+              var a = $scope.employees = $wakanda.$ds.Employee.$find({
+                filter: 'firstName = _*',
+                select:'employer'
               });
-              $scope.companies = $wakanda.$ds.Company.$find({
-                filter:'name = _*'
+              var b = $scope.companies = $wakanda.$ds.Company.$find({
+                filter: 'name = _*',
+                select: 'staff'//jump to employees collection
               });
             };
-            
-            for(i=0; i<10; i++){
-              employeesToSave.push($wakanda.$ds.Employee.$create({
-                firstName:'_foo'+i,lastName:'_bar'+i
-              }));
+
+            //just One then
+            myNewCompany = $wakanda.$ds.Company.$create({name: '_comp' + (new Date()).getTime()});
+
+            for (i = 0; i < 10; i++) {
+              myNewEmployeeTmp = $wakanda.$ds.Employee.$create({firstName: '_foo' + i, lastName: '_bar' + i});
+
+              //There goes the bind sorry it was in doing some stuff I think that's the cause
+              myNewEmployeeTmp.employer = myNewCompany;
+              myNewEmployeeTmp.$_entity.employer.setValue(myNewEmployeeTmp.employer.$_entity);
+
+              myNewEmployeeCollection.push(myNewEmployeeTmp);
             }
-            for(i=0; i<10; i++){
-              companiesToSave.push($wakanda.$ds.Company.$create({
-                name:'_comp'+i
-              }));
-            }
-            
+
             function saveCollection(collection) {
-              return _.map(collection,function(entity) {
+              return _.map(collection, function (entity) {
                 //no need for this fix at all
 //                if (entity.$_entity.isNew()) {
 //                  delete entity.ID;
@@ -37,15 +43,17 @@ angular.module('angularWakandaFrontApp')
                 return entity.$save();
               });
             }
-            
-            $scope.launchSaveCollection = function() {
-              return $q.all(saveCollection(employeesToSave)).then(function() {
-                return $q.all(saveCollection(companiesToSave)).then(function(){
-                  $scope.display();
-                });
+
+            $scope.launchSaveCollection = function () {
+
+              myNewCompany.$save().then(function () {
+                $q.all(saveCollection(myNewEmployeeCollection))
+                  .then(function () {
+                          console.log('Confirm the save collection of employees promise is resolved');
+                        });
               });
             };
-            
+
             $scope.display();
-            
-        }]);
+
+          }]);
