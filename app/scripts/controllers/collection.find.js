c5176e382c65f82d2671910933a4d9e305af2cc4
Christophe Rosset <tophe@topheman.com>
2015-02-09 15:55:46 +0100

    collection.$find()

--- app/scripts/controllers/collection.find.js
+++ app/scripts/controllers/collection.find.js
@@ -9,10 +9,34 @@
  */
 angular.module('angularWakandaFrontApp')
   .controller('CollectionFindCtrl', function ($scope, $wakanda) {
-    $scope.employees1 = $wakanda.$ds.Employee.$find({
-      pageSize : 10
-    });
-    $scope.employees2 = $wakanda.$ds.Employee.$find({
-      pageSize : 10
-    });
+    
+    $scope.filterEmployees1 = 'employer.name="Calendar Sable Diffusing Ring"';
+    $scope.filterEmployees2 = "salary>90000";
+
+    $scope.launchFindEmployees1 = function(){
+      window.employees1 = $scope.employees1 = $wakanda.$ds.Employee.$find({
+        pageSize : 10,
+        select : 'employer',
+        filter : $scope.filterEmployees1
+      });
+      $scope.employees1.$promise.then(function(){
+        $scope.launchFindEmployees2();
+      });
+    };
+
+    $scope.launchFindEmployees2 = function(){
+      window.employees2 = $scope.employees2 = $scope.employees1.$find({
+        filter : $scope.filterEmployees2,
+        orderBy : 'salary desc'
+      });
+    };
+
+    $scope.callEntityMethod = function(employee){
+      employee.myEntityMethod().then(function(e){
+        employee.message = e.result;
+      });
+    };
+    
+    $scope.launchFindEmployees1();
+
   });
