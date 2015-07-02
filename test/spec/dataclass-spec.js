ccbc18e3796d1c1fef25bd09293491c78d41af36
yassine boukhata <yacine_r@yahoo.fr>
2015-07-02 17:13:56 +0200

    use $promise instead setTimeout & code refactor & improve time execution

--- test/spec/dataclass-spec.js
+++ test/spec/dataclass-spec.js
@@ -1,189 +1,148 @@
 describe('Connector/Dataclass:', function() {
   var $wakanda, $rootScope, $q, unitTestsHelpers,
-    intervalRef;
+  intervalRef,
+  employees, employee,
+  ds;
 
-  beforeEach(function(){
-    module('wakanda');
-    module('unitTestsHelpersModule');
+  beforeEach(function() {
+    if(! $wakanda) {
+      module('wakanda');
+      module('unitTestsHelpersModule');
+      inject(function(_$rootScope_, _$wakanda_, _$q_, _unitTestsHelpers_) {
+          $q = _$q_;
+          $rootScope = _$rootScope_;
+          $wakanda = _$wakanda_;
+          unitTestsHelpers = _unitTestsHelpers_;
+          unitTestsHelpers.db.reset(false);
+          // https://github.com/domenic/chai-as-promised/issues/68
+          intervalRef = setInterval(function(){ $rootScope.$apply(); }, 1);
+      });
+    }
   });
 
-  beforeEach(inject(function(_$rootScope_, _$wakanda_, _$q_, _unitTestsHelpers_) {
-    $q = _$q_;
-    $rootScope = _$rootScope_;
-    $wakanda = _$wakanda_;
-    unitTestsHelpers = _unitTestsHelpers_;
-    unitTestsHelpers.db.reset(false);
-    // https://github.com/domenic/chai-as-promised/issues/68
-    intervalRef = setInterval(function(){ $rootScope.$apply(); }, 1);
-  }));
+  beforeEach(function(done) {
+    if(ds) {
+      done();
+    } else {
+      $wakanda.init().then(function(dataStore) {
+        ds = dataStore;
+        done();
+      });
+    }
+  });
 
   describe('$create() function', function() {
     it('should return the name defined for the datastore class in the Model', function(done) {
-      $wakanda.init().then(function(){
-        var newPerson = { firstName : "John", lastName : "Smith", salary: 90000 };
-        var person = $wakanda.$ds.Employee.$create( newPerson );
-        expect(person).to.be.a('object');
-        expect(person.firstName).to.be.equal(newPerson.firstName);
-        expect(person.lastName).to.be.equal(newPerson.lastName);
-        expect(person.salary).to.be.equal(newPerson.salary);
-        person.$save().should.notify(done);
-      });
+      var newPerson = { firstName : "John", lastName : "Smith", salary: 90000 };
+      var person = $wakanda.$ds.Employee.$create( newPerson );
+      expect(person).to.be.a('object');
+      expect(person.firstName).to.be.equal(newPerson.firstName);
+      expect(person.lastName).to.be.equal(newPerson.lastName);
+      expect(person.salary).to.be.equal(newPerson.salary);
+      person.$save().should.notify(done);
     });
   });
 
   describe('$find() function', function() {
     it('should return the Entity collection of the records found in the Model', function(done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 20
-        });
-        setTimeout(function(){
-          expect(employees).to.be.an('array');
-          expect(employees.length).to.be.equal(20);
-          done();
-        },250);
+      var employees = $wakanda.$ds.Employee.$find({
+        filter: 'lastName > :1 && salary > :2',
+        params: ['a*', 60000],
+        orderBy: 'firstName desc',
+        pageSize: 20
+      });
+      employees.$promise.then(function() {
+        expect(employees).to.be.an('array');
+        expect(employees.length).to.be.equal(20);
+        done();
       });
     });
   });
 
   describe('dataClass.$name variable', function() {
     it('should return the name defined for the datastore class in the Model', function(done) {
-      $wakanda.init().then(function(){
-        var collectionName = $wakanda.$ds.Employee.$name;
-        setTimeout(function(){
-          expect(collectionName).to.be.a('string');
-          expect(collectionName).to.be.equal('Employee');
-          done();
-        },250);
-      });
+      var collectionName = $wakanda.$ds.Employee.$name;
+      expect(collectionName).to.be.a('string');
+      expect(collectionName).to.be.equal('Employee');
+      done();
     });
   });
 
   describe('dataClass.$collectionName variable', function() {
     it('should return the name defined for the collection of the datastore class in the Model', function(done) {
-      $wakanda.init().then(function(){
-        var collectionName = $wakanda.$ds.Employee.$collectionName;
-        setTimeout(function(){
-          expect(collectionName).to.be.a('string');
-          expect(collectionName).to.be.equal('EmployeeCollection');
-          done();
-        },250);
-      });
+      var collectionName = $wakanda.$ds.Employee.$collectionName;
+      expect(collectionName).to.be.a('string');
+      expect(collectionName).to.be.equal('EmployeeCollection');
+      done();
     });
   });
 
   describe('dataClass.$dataClassMethods() function', function() {
     it('should return the dataclass\'s datastore class methods whose scope is public', function(done) {
-      $wakanda.init().then(function(){
-        var dataClassMethods = $wakanda.$ds.Employee.$dataClassMethods();
-        setTimeout(function(){
-          expect(dataClassMethods).to.be.an('array');
-          done();
-        },250);
-      });
+      var dataClassMethods = $wakanda.$ds.Employee.$dataClassMethods();
+      expect(dataClassMethods).to.be.an('array');
+      done();
     });
   });
 
   describe('dataClass.$collectionMethods() function', function() {
     it('should return the collection\'s datastore class methods whose scope is public', function(done) {
-      $wakanda.init().then(function(){
-        var collectionMethods = $wakanda.$ds.Employee.$collectionMethods();
-        setTimeout(function(){
-          expect(collectionMethods).to.be.an('array');
-          $wakanda.$ds.Employee.myDataClassMethod('myDataClassMethod').then(function(e){
-            expect(e.result).to.be.defined;
-            done();
-          });
-        },250);
+      var collectionMethods = $wakanda.$ds.Employee.$collectionMethods();
+      expect(collectionMethods).to.be.an('array');
+      $wakanda.$ds.Employee.myDataClassMethod('myDataClassMethod').then(function(e){
+        expect(e.result).to.be.defined;
+        done();
       });
     });
   });
 
   describe('dataClass.$entityMethods() function', function() {
     it('should return the dataclass\'s entity methods whose scope is public', function(done) {
-      $wakanda.init().then(function(){
-        var entityMethods = $wakanda.$ds.Employee.$entityMethods();
-        setTimeout(function(){
-          expect(entityMethods).to.be.an('array');
-          done();
-        },250);
-      });
+      var entityMethods = $wakanda.$ds.Employee.$entityMethods();
+      expect(entityMethods).to.be.an('array');
+      done();
     });
     it('should return a specific dataclass\'s entity method whose scope is public', function(done) {
-      $wakanda.init().then(function(){
-        var entityMethods = $wakanda.$ds.Employee.$entityMethods();
-        setTimeout(function(){
-          expect(entityMethods).to.be.an('array');
-          var entityMethod = $wakanda.$ds.Employee.$entityMethods(entityMethods[0]);
-          setTimeout(function(){
-            expect(entityMethod).to.be.an('array');
-            done();
-          },250);
-        },250);
-      });
+      var entityMethods = $wakanda.$ds.Employee.$entityMethods();
+      expect(entityMethods).to.be.an('array');
+      var entityMethod = $wakanda.$ds.Employee.$entityMethods(entityMethods[0]);
+      expect(entityMethod).to.be.an('array');
+      done();
     });
   });
 
   describe('dataClass.$attr(name) function', function() {
     it('should return an attribute from a dataclass', function(done) {
-      $wakanda.init().then(function(){
-        var dataClassAttribute = $wakanda.$ds.Employee.$attr('firstName');
-        setTimeout(function(){
-          expect(dataClassAttribute).to.be.an('object');
-          expect(dataClassAttribute.name).to.be.equal('firstName');
-          done();
-        },250);
-      });
+      var dataClassAttribute = $wakanda.$ds.Employee.$attr('firstName');
+      expect(dataClassAttribute).to.be.an('object');
+      expect(dataClassAttribute.name).to.be.equal('firstName');
+      done();
     });
     it('should return an null when attribute from a dataclass is not found', function(done) {
-      $wakanda.init().then(function(){
-        var dataClassAttribute = $wakanda.$ds.Employee.$attr('geronimo');
-        setTimeout(function(){
-          expect(dataClassAttribute).to.be.null;
-          done();
-        },250);
-      });
+      var dataClassAttribute = $wakanda.$ds.Employee.$attr('geronimo');
+      expect(dataClassAttribute).to.be.null;
+      done();
     });
     it('should return all the attributes when no (name) is specified', function(done) {
-      $wakanda.init().then(function(){
-        var dataClassAttribute = $wakanda.$ds.Employee.$attr();
-        setTimeout(function(){
-          expect(dataClassAttribute).to.be.an('object');
-          done();
-        },250);
-      });
+      var dataClassAttribute = $wakanda.$ds.Employee.$attr();
+      expect(dataClassAttribute).to.be.an('object');
+      done();
     });
   });
 
   describe('dataClass.$_relatedAttributes(name) function', function() {
     it('should return the related attributes from a dataclass', function(done) {
-      $wakanda.init().then(function(){
-        var _relatedAttributes = $wakanda.$ds.Employee.$_relatedAttributes;
-        setTimeout(function(){
-          expect(_relatedAttributes).to.be.an('array');
-          done();
-        },250);
-      });
+      var _relatedAttributes = $wakanda.$ds.Employee.$_relatedAttributes;
+      expect(_relatedAttributes).to.be.an('array');
+      done();
     });
   });
 
   describe('dataClass.$_processedAttributes(name) variable', function() {
     it('should return the processed attributes from a dataclass', function(done) {
-      $wakanda.init().then(function(){
-        var _processedAttributes = $wakanda.$ds.Employee.$_processedAttributes;
-        setTimeout(function(){
-          expect(_processedAttributes).to.be.an('array');
-          done();
-        },250);
-      });
+      var _processedAttributes = $wakanda.$ds.Employee.$_processedAttributes;
+      expect(_processedAttributes).to.be.an('array');
+      done();
     });
-  });
-
-  afterEach(function() {
-    $wakanda = $rootScope = null;
-    clearInterval(intervalRef);
   });
 });
--- test/spec/entitycollection-spec.js
+++ test/spec/entitycollection-spec.js
@@ -1,319 +1,198 @@
 describe('Connector/EntityCollection:', function() {
   var $wakanda, $rootScope, $q, unitTestsHelpers,
-    intervalRef;
+    intervalRef,
+    employees,
+    ds;
 
-  beforeEach(function(){
-    module('wakanda');
-    module('unitTestsHelpersModule');
+  beforeEach(function() {
+    if(! $wakanda) {
+      module('wakanda');
+      module('unitTestsHelpersModule');
+      inject(function(_$rootScope_, _$wakanda_, _$q_, _unitTestsHelpers_) {
+          $q = _$q_;
+          $rootScope = _$rootScope_;
+          $wakanda = _$wakanda_;
+          unitTestsHelpers = _unitTestsHelpers_;
+          unitTestsHelpers.db.reset(false);
+          // https://github.com/domenic/chai-as-promised/issues/68
+          intervalRef = setInterval(function(){ $rootScope.$apply(); }, 1);
+      });
+    }
   });
 
-  beforeEach(inject(function(_$rootScope_, _$wakanda_, _$q_, _unitTestsHelpers_) {
-    $q = _$q_;
-    $rootScope = _$rootScope_;
-    $wakanda = _$wakanda_;
-    unitTestsHelpers = _unitTestsHelpers_;
-    unitTestsHelpers.db.reset(false);
-    // https://github.com/domenic/chai-as-promised/issues/68
-    intervalRef = setInterval(function(){ $rootScope.$apply(); }, 1);
-  }));
+  beforeEach(function(done) {
+    if(ds) {
+      done();
+    } else {
+      $wakanda.init().then(function(dataStore) {
+        ds = dataStore;
+        done();
+      });
+    }
+  });
+
+  beforeEach(function(done) {
+    employees = $wakanda.$ds.Employee.$find({
+      filter: 'lastName > :1 && salary > :2',
+      params: ['a*', 60000],
+      orderBy: 'firstName desc',
+      pageSize: 5
+    });
+    employees.$promise.then(function() {
+      done();
+    });
+  });
 
   describe('$totalCount variable', function() {
     it('should return number of entities in the collection', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          expect(employees.$totalCount).to.be.a('number');
-          expect(employees.$totalCount).to.be.gt(0);
-          done();
-        },250);
-      });
+      expect(employees.$totalCount).to.be.a('number');
+      expect(employees.$totalCount).to.be.gt(0);
+      done();
     });
   });
 
   describe('$query variable', function() {
     it('should have information about the query', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          expect(employees.$query.pageSize).to.be.a('number');
-          expect(employees.$query.pageSize).to.be.equal(5);
-          expect(employees.$query.start).to.be.a('number');
-          expect(employees.$query.start).to.be.equal(0);
-          expect(employees.$query.filter).to.be.defined;
-          done();
-        },250);
-      });
+      expect(employees.$query.pageSize).to.be.a('number');
+      expect(employees.$query.pageSize).to.be.equal(5);
+      expect(employees.$query.start).to.be.a('number');
+      expect(employees.$query.start).to.be.equal(0);
+      expect(employees.$query.filter).to.be.defined;
+      done();
     });
   });
 
   describe('$fetch() function', function() {
     it('should fetch the data', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          employees.$fetch({ start: 1, params: ['a*', 5000] }).should.be.fulfilled.then(function(fetchResult){
-            expect(fetchResult).to.be.an('object');
-            done();
-          });
-        },250);
+      employees.$fetch({ start: 1, params: ['a*', 5000] }).should.be.fulfilled.then(function(fetchResult){
+        expect(fetchResult).to.be.an('object');
+        done();
       });
     });
     it('should refresh when empty', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          employees.$fetch().should.be.fulfilled.then(function(fetchResult){
-            expect(fetchResult).to.be.an('object');
-            done();
-          });
-        },250);
+      employees.$fetch().should.be.fulfilled.then(function(fetchResult){
+        expect(fetchResult).to.be.an('object');
+        done();
       });
     });
     it('should throw an error when select can\'t be change on a $fetch', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          try {
-            employees.$fetch({ select: 'staff' });
-            Assert.Fail();
-          } catch (Exception) {
-            expect(Exception).to.be.an.instanceof(Error);
-            done();
-          }
-        },250);
-      });
+      try {
+        employees.$fetch({ select: 'staff' });
+      } catch (Exception) {
+        expect(Exception).to.be.an.instanceof(Error);
+        done();
+      }
     });
     it('should throw an error when orderBy can\'t be change on a $fetch', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          try {
-            employees.$fetch({ orderBy: 'firstName asc' });
-            Assert.Fail();
-          } catch (Exception) {
-            expect(Exception).to.be.an.instanceof(Error);
-            done();
-          }
-        },250);
-      });
+      try {
+        employees.$fetch({ orderBy: 'firstName asc' });
+      } catch (Exception) {
+        expect(Exception).to.be.an.instanceof(Error);
+        done();
+      }
     });
     it('should accept other params like start and params', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          employees.$fetch({ start: 1, params: ['a*', 5000] }).should.be.fulfilled.then(function(fetchResult){
-            expect(fetchResult).to.be.an('object');
-            done();
-          });
-        },250);
+      employees.$fetch({ start: 1, params: ['a*', 5000] }).should.be.fulfilled.then(function(fetchResult){
+        expect(fetchResult).to.be.an('object');
+        done();
       });
     });
     it('should work in replace mode', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          employees.$fetch({ start: 1, params: ['a*', 5000] },'replace').should.be.fulfilled.then(function(fetchResult){
-            expect(fetchResult).to.be.an('object');
-            done();
-          });
-        },250);
+      employees.$fetch({ start: 1, params: ['a*', 5000] },'replace').should.be.fulfilled.then(function(fetchResult){
+        expect(fetchResult).to.be.an('object');
+        done();
       });
     });
     it('should work in append mode', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          employees.$fetch({ start: 1, params: ['a*', 5000] },'append').should.be.fulfilled.then(function(fetchResult){
-            expect(fetchResult).to.be.an('object');
-            done();
-          });
-        },250);
+      employees.$fetch({ start: 1, params: ['a*', 5000] },'append').should.be.fulfilled.then(function(fetchResult){
+        expect(fetchResult).to.be.an('object');
+        done();
       });
     });
     it('should throw an error with unknown modes', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          try {
-            employees.$fetch({ start: 1 },'geronimoMode');
-            Assert.Fail();
-          } catch (Exception) {
-            expect(Exception).to.be.an.instanceof(Error);
-            done();
-          }
-        },250);
-      });
+      try {
+        employees.$fetch({ start: 1 }, 'geronimoMode');
+      } catch (Exception) {
+        expect(Exception).to.be.an.instanceof(Error);
+        done();
+      }
     });
   });
 
   describe('$fetching variable', function() {
-    it('should return an Entity relation', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 500
-        });
-        expect(employees.$fetching).to.be.true;
-        setTimeout(function(){
-          expect(employees.$fetching).to.be.false;
-          done();
-        },500);
+    it('should return fetching status', function (done) {
+      var employees = $wakanda.$ds.Employee.$find();
+      expect(employees.$fetching).to.be.true;
+      employees.$promise.then(function() {
+        expect(employees.$fetching).to.be.false;
+        done();
       });
     });
   });
 
   describe('$isLoaded() function', function() {
     it('should return the status of a query', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          employees.$fetch('employer');
-          expect(employees['0'].employer.$isLoaded()).to.be.false;
-          done();
-        },250);
-      });
+      employees.$fetch('employer');
+      expect(employees['0'].employer.$isLoaded()).to.be.false;
+      done();
     });
   });
 
   describe('$more() function', function() {
     it('should retrieve more data', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          filter: 'lastName > :1 && salary > :2',
-          params: ['a*', 60000],
-          orderBy: 'firstName desc',
-          pageSize: 5
-        });
-        setTimeout(function(){
-          expect(employees.length).to.be.equal(5);
-          employees.$more();
-          setTimeout(function(){
-            expect(employees.length).to.be.equal(10);
-            done();
-          },500);
-        },250);
+      expect(employees.length).to.be.equal(5);
+      employees.$more().then(function() {
+        expect(employees.length).to.be.equal(10);
+        done();
       });
     });
   });
 
   describe('$nextPage() function', function() {
     it('should retrieve more and different data', function (done) {
-      $wakanda.init().then(function(){
         var employees = $wakanda.$ds.Employee.$find({
           pageSize: 10
         });
-        setTimeout(function(){
+        employees.$promise.then(function() {
           var employeeFirstPage = employees[0];
           expect(employees.length).to.be.equal(10);
-          employees.$nextPage();
-          setTimeout(function(){
+          employees.$nextPage().then(function() {
             var employeeSecondPage = employees[0];
             expect(employees.length).to.be.equal(10);
             expect(employeeFirstPage.firstName).to.be.not.equal(employeeSecondPage.firstName);
             done();
-          },500);
-        },250);
-      });
+          });
+        });
     });
   });
 
   describe('$prevPage() function', function() {
     it('should retrieve more and different data', function (done) {
-      $wakanda.init().then(function(){
-        var employees = $wakanda.$ds.Employee.$find({
-          pageSize: 10
+      var employees = $wakanda.$ds.Employee.$find({
+        pageSize: 10
+      });
+      employees.$promise.then(function() {
+        var employeeFirstPage = employees[0];
+        expect(employees.length).to.be.equal(10);
+        employees.$nextPage().then(function() {
+          var employeeSecondPage = employees[0];
+          employees.$prevPage().then(function() {
+            var employeeThirdPage = employees[0];
+            expect(employees.length).to.be.equal(10);
+            expect(employeeFirstPage.firstName).to.be.not.equal(employeeSecondPage.firstName);
+            expect(employeeFirstPage.firstName).to.be.equal(employeeThirdPage.firstName);
+            done();
+          });
         });
-        setTimeout(function(){
-          var employeeFirstPage = employees[0];
-          expect(employees.length).to.be.equal(10);
-          employees.$nextPage();
-          setTimeout(function(){
-            var employeeSecondPage = employees[0];
-            employees.$prevPage();
-            setTimeout(function(){
-              var employeeThirdPage = employees[0];
-              expect(employees.length).to.be.equal(10);
-              expect(employeeFirstPage.firstName).to.be.not.equal(employeeSecondPage.firstName);
-              expect(employeeFirstPage.firstName).to.be.equal(employeeThirdPage.firstName);
-              done();
-            },500);
-          },500);
-        },250);
       });
     });
   });
 
   describe('$Entity variable', function() {
     it('should return a $Entity object', function (done) {
-      $wakanda.init().then(function(){
-        var Entity = $wakanda.$ds.Employee.$Entity;
-        setTimeout(function(){
-          expect(Entity).to.be.an('object');
-          done();
-        },150);
-      });
+      var Entity = $wakanda.$ds.Employee.$Entity;
+      expect(Entity).to.be.an('object');
+      done();
     });
-  });
-
-  afterEach(function() {
-    $wakanda = $rootScope = null;
-    clearInterval(intervalRef);
   });
 });
